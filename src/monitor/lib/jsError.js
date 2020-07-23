c3a91894e6511cc9a84f9ae5297aa79800097943
e <812974019@qq.com>
2020-07-23 11:35:24 +0800

    解决上传日志到阿里云服务器接口400的问题

--- src/monitor/lib/jsError.js
+++ src/monitor/lib/jsError.js
@@ -1,30 +1,29 @@
 // 监控js错误
 // 导出一个脚本函数
-import getLastEvent from '../utils/getLastEvent' 
+import getLastEvent from '../utils/getLastEvent'
 import getSelector from '../utils/getSelector'
-import SendTracker from '../utils/tracker' 
-export function injectJsError() {
+import SendTracker from '../utils/tracker'
+export function injectJsError () {
     // 监听全局未捕获的错误
-    window.addEventListener('error', function(event) {
+    window.addEventListener('error', function (event) {
         let lastEvent = getLastEvent() // 最后一个交互事件
-        console.log(lastEvent)
+        // console.log(lastEvent)
         // event 错误事件对象之一
         let log = {
             kind: 'stability', // 监控指标的大类
-             type: 'error', // 小类型，这是一个错误
-             errorType: 'jsError', // js执行错误
-             url: '', // 访问哪个路径 报错了
-             message: event.filename, // 报错信息
-             filename: event.filename, // 哪个文件报错了
-             position: `${event.lineno}: ${event.colno}`,
-             stack: getLines(event.error.stack),
-             selector:  lastEvent ? getSelector(lastEvent.path) : '' // 代表最后一个操作的元素
+            type: 'error', // 小类型，这是一个错误
+            errorType: 'jsError', // js执行错误
+            url: '', // 访问哪个路径 报错了
+            message: event.message, // 报错信息
+            filename: event.filename, // 哪个文件报错了
+            position: `${event.lineno}: ${event.colno}`,
+            stack: getLines(event.error.stack),
+            selector: lastEvent ? getSelector(lastEvent.path) : '' // 代表最后一个操作的元素
         }
-        console.log(log)
         SendTracker.send(log)
     })
 
-    function getLines(stack) {
+    function getLines (stack) {
         return stack.split('\n').slice(1).map(item => item.replace(/^\s+at\s+/g, '')).join('^')
     }
 }
--- src/monitor/utils/tracker.js
+++ src/monitor/utils/tracker.js
@@ -1,14 +1,14 @@
 let host = 'cn-shanghai.log.aliyuncs.com' // 主机名
-let project = 'lhr-monitor'
-let logstoreName = 'lhr-monitor-store'
+let project = 'lhrmonitor'
+let logstoreName = 'lhrmonitorstore'
 let userAgent = require('user-agent')
-function getExtraData() {
+function getExtraData () {
     return {
         // 用户id， 用户token等...
         title: document.title,
         url: location.url,
         timestemp: Date.now(),
-        userAgent: userAgent.parse(navigator.userAgent)
+        userAgent: JSON.stringify(userAgent.parse(navigator.userAgent))
     }
 }
 
@@ -16,30 +16,34 @@ function getExtraData() {
 class SendTracker {
     constructor() {
         this.url = `http://${project}.${host}/logstores/${logstoreName}/track`
-        this.xhr = new XMLHttpRequest
+        this.xhr = new XMLHttpRequest();
     }
-    send(data = {}) {
+    send (data = {}) {
         let extraData = getExtraData()
-        let log = {...extraData, ...data}
+        let logs = { ...extraData, ...data }
         // 坑，对象的值不能是数字
-        for(let key in log) {
-            if(typeof log[key] === 'number') {
-                log[key] = `${log[key]}`
+        for (let key in logs) {
+            if (typeof logs[key] === 'number') {
+                logs[key] = "" + logs[key];
             }
         }
-        console.log(log)
+        // 这里规定， send的 数据的__log__的值是一个数组，数组中的值是对象，该对象中的每个值都必须是字符串，如果不是字符串将会报错
+        // https://help.aliyun.com/document_detail/120218.html?spm=a2c4g.11186623.4.1.4aa15ad13kqlfz
+        let body = JSON.stringify({
+            __logs__: [JSON.parse(JSON.stringify(logs))]
+        });
         this.xhr.open('POST', this.url, true)
-        let body = JSON.stringify(log)
-        this.xhr.setRequestHeader('Content-Type','application/json')  // 请求体类型
-        this.xhr.setRequestHeader('x-log-apiversion','0.6.0') // 版本号
-        this.xhr.setRequestHeader('x-log-bodyrawsize',body.length) // 请求体大小
+        // let body = JSON.stringify(logs)
+        this.xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");// 请求体类型
+        this.xhr.setRequestHeader('x-log-apiversion', '0.6.0'); // 版本号
+        this.xhr.setRequestHeader('x-log-bodyrawsize', body.length);// 请求体大小
         this.xhr.onload = function () {
             // console.log('this.xhr.response', this.xhr.response)
         }
-        this.xhr.onerror = function(error) {
+        this.xhr.onerror = function (error) {
             // console.log('error', error)
         }
-        this.xhr.send()
+        this.xhr.send(body)
     }
 }
 
